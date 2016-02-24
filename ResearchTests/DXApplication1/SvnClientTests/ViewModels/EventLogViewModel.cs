164e494a6a336ed86f464b93ca37d03010eab8d8
Andreas Patock <apatock@googlemail.com>
2016-02-24 21:43:19 +0100

    Erster Entwurf

---
+++ ResearchTests/DXApplication1/SvnClientTests/ViewModels/EventLogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm;
+
+namespace SvnClientTests.ViewModels
+{
+    [POCOViewModel]
+    public class EventLogViewModel
+    {
+    }
+}
