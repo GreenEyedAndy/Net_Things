8d7fd3de6009c8adafdf04b75dc2eb5a17c3efa4
DocPatock <apatock@gmx.de>
2016-02-25 09:00:41 +0100

    EventLogView

--- ResearchTests/DXApplication1/SvnClientTests/ViewModels/EventLogViewModel.cs
+++ ResearchTests/DXApplication1/SvnClientTests/ViewModels/EventLogViewModel.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using DevExpress.Mvvm.DataAnnotations;
 using DevExpress.Mvvm;
+using DevExpress.Mvvm.POCO;
 
 namespace SvnClientTests.ViewModels
 {
     [POCOViewModel]
     public class EventLogViewModel
     {
+        public EventLogViewModel()
+        {
+            if (this.IsInDesignMode())
+            {
+                InitializeDesignTimeData();
+            }
+        }
+
+        private void InitializeDesignTimeData()
+        {
+            LogText = string.Empty;
+            AddText("Text1");
+            AddText("Text2");
+            AddText("Text3");
+        }
+
+        public virtual string LogText { get; set; }
+
+        public void AddText(string newText)
+        {
+            LogText += newText + Environment.NewLine;
+        }
     }
 }
